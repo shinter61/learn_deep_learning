afe1de43114feac09cd025e79d52eae47aa00eeb
shinter61 <shinter61@gmail.com>
2021-12-03 23:00:44 +0900

    11/26

--- grammer.py
+++ grammer.py
@@ -98,3 +98,47 @@ man1.hello()
 #     def is_integer():
 #         return True 
 
+
+import numpy as np
+
+x = np.array([1.0,2.0,3.0])
+y = np.array([2.0,4.0,6.0])
+
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+
+
+# print(x/2.0)
+
+
+A = np.array([[1,2],[3,4]])
+# print(A)
+# print(A.shape)
+# print(A.dtype)
+
+B = np.array([[3,0],[0,6]])
+# print(A + B)
+# print(A * B)
+# print(A * 10)
+
+C = np.array([10,20]) # => [[10, 20], [10, 20]]
+10 # => [[10,10], [10, 10]]
+print(A*C)
+
+X = np.array([[51,55],[14,19],[0,4]])
+print(X)
+
+print(X[0])
+
+for row in X:
+    print(row)
+X = X.flatten()
+print(X)
+print(X[np.array([0,2,4])])    
+print(X[np.array([1,2,3])])
+
+print(X>15)
+print(X[X>15])
+print(X[np.array([True, True, False, True, False, True])])
